3b2ec2a8a81b1a91071330a305aeda6ea283c7eb
Camilo Dominguez <camiloa17@gmail.com>
2019-04-26 01:31:04 -0300

    mejore funcionalidad del jugador(se agrega la opcion de cambiar imagen y texto del dom y se guarda el valor en la variable escogeUsuario), se mejoro la funcion opcionPC(se cambia el dom de acuerdo a lo que aleatoreamente se escoge del array texto e imagen, se guarda el valor en la variable escogePC), cambie nombre de la variable submit para iniciar y dentro de ella se ejecuta opcionPc y ver si gano. Se agrega evento de click en el boton para iniciar la funcion iniciar.

--- index.js
+++ index.js
@@ -3,29 +3,51 @@ let array=[
     'papel',
     'tijera'    
 ]
+let escogeUsuario; 
+let escogePc;
+document.querySelector("#buttonSubmit").addEventListener("click", iniciar);
 
-document.querySelector("#buttonSubmit").addEventListener("click", iniciarDespuesDelSumitted);
 
-function iniciarDespuesDelSumitted () {
-    verSiGano (jugador(), opcionPc () );
+
+
+function iniciar () {
+    opcionPc();
+    verSiGano (escogeUsuario, escogePc );
+    console.log(escogeUsuario, escogePc);
 }
 
-
+// Selecciona la opcion de la computadora y cambiar imagen y text del DOM de acuerdo a lo que escoge
 function opcionPc() {
     let opcionPc = Math.floor(Math.random()*3);
-    return array[opcionPc];
+    //Modifica texto del dom de acuerdo a la jugada
+    document.querySelector("#jugadaPC").innerHTML = array[opcionPc];
+    //modifica la imagen del dom de acuerto a la jugada
+    document.querySelector(".computador img").src = "imagenes/" + array[opcionPc] + ".png";
+    escogePc = array[opcionPc];
+
     
 }
 
-
+// Selecciona la opcion del jugador y cambiar imagen y text del DOM de acuerdo a lo que escoge
 function jugador(){
-    let jugadorSelect = document.querySelectorAll("input")
-    for(let select of jugadorSelect){
-        if(select.checked == true){
-            return select.value;
-        }
+    let allInputs = document.querySelectorAll("input")
+    for(let input of allInputs){
+        input.addEventListener("click", function(event){
+            //Obtiene el valor del input clickeado y se guarda para utilizar
+           let value = event.target.value;
+           //Modifica texto del dom de acuerdo a la jugada
+            document.querySelector("#jugadaJugador").innerHTML = value;
+
+           //modifica la imagen del dom de acuerto a la jugada
+            document.querySelector(".jugador img").src ="imagenes/"+ value + ".png";
+            escogeUsuario=value;
+        })   
     }
 }
+
+// Se ejecuta la funcion de jugador
+jugador()
+
 
 
 function verSiGano(opJug, opPC) {
@@ -45,7 +67,3 @@ function verSiGano(opJug, opPC) {
     }
 }
 
-function modificarDOM() {
-    console.log("modifica dom????!!!!");
-    
-}
