52d1fe83e8716b4652b5d643951923a058438b34
Nahir Ayala <nayalabarrios@hotmail.com>
2019-04-25 22:04:06 -0300

    agregar validacion

--- index.js
+++ index.js
@@ -5,20 +5,40 @@ let array=[
 ]
 let opcionelegida;
 
+
+
+function iniciarDespuesDelSumitted () {
+    verSiGano (opcionJugador (), opcionPc () );
+
+}
+
 function opcionJugador() {
     return("piedra");
     
 }
 
 
-function pc() {
+function opcionPc() {
     let opcionPc = Math.floor(Math.random()*3);
     array[opcionPc];
     console.log(array[opcionPc])
 }
 
-function verSiGano() {
-    return("GANASTE");
+function verSiGano(opJug, opPC) {
+    switch (opPC) {
+        case opJug:
+            console.log ("empate");
+            break;
+        case "piedra":
+            
+        break;
+        case "tijera":
+            
+        break;
+    
+        default:
+            break;
+    }
 }
 
 function modificarDOM() {
