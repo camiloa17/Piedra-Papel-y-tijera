ac59069788bcfc7b200e8daaccfcc077062ecb44
Nahir Ayala <nayalabarrios@hotmail.com>
2019-04-25 21:38:26 -0300

    js esqueleto

--- index.js
+++ index.js
@@ -5,8 +5,23 @@ let array=[
 ]
 let opcionelegida;
 
+function opcionJugador() {
+    return("piedra");
+    
+}
+
+
 function pc() {
     let opcionPc = Math.floor(Math.random()*3);
     array[opcionPc];
     console.log(array[opcionPc])
+}
+
+function verSiGano() {
+    return("GANASTE");
+}
+
+function modificarDOM() {
+    console.log("modifica dom????!!!!");
+    
 }
