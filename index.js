db29ed6a6aad5fa773af7a64a6fce60a39f4a937
Camilo Dominguez <camiloa17@gmail.com>
2019-04-27 11:32:04 -0300

    agregue logica de ganar y cambio del color de fondo si gana o pierde

--- index.js
+++ index.js
@@ -1,48 +1,47 @@
-let array=[
+let array = [
     'piedra',
     'papel',
-    'tijera'    
+    'tijera'
 ]
 
-let escogeUsuario; 
+let escogeUsuario;
 let escogePc;
 document.querySelector("#buttonSubmit").addEventListener("click", iniciar);
 
 
 
 
-function iniciar () {
+function iniciar() {
     opcionPc();
-    verSiGano (escogeUsuario, escogePc );
-    console.log(escogeUsuario, escogePc);
+    verSiGano(escogeUsuario, escogePc);
 }
 
 // Selecciona la opcion de la computadora y cambiar imagen y text del DOM de acuerdo a lo que escoge
 function opcionPc() {
-    let opcionPc = Math.floor(Math.random()*3);
+    let opcionPc = Math.floor(Math.random() * 3);
     //Modifica texto del dom de acuerdo a la jugada
     document.querySelector("#jugadaPC").innerHTML = array[opcionPc];
     //modifica la imagen del dom de acuerto a la jugada
     document.querySelector(".computador img").src = "imagenes/" + array[opcionPc] + ".png";
     escogePc = array[opcionPc];
 
-    
+
 }
 
 // Selecciona la opcion del jugador y cambiar imagen y text del DOM de acuerdo a lo que escoge
-function jugador(){
+function jugador() {
     let allInputs = document.querySelectorAll("input")
-    for(let input of allInputs){
-        input.addEventListener("click", function(event){
+    for (let input of allInputs) {
+        input.addEventListener("click", function (event) {
             //Obtiene el valor del input clickeado y se guarda para utilizar
-           let value = event.target.value;
-           //Modifica texto del dom de acuerdo a la jugada
+            let value = event.target.value;
+            //Modifica texto del dom de acuerdo a la jugada
             document.querySelector("#jugadaJugador").innerHTML = value;
 
-           //modifica la imagen del dom de acuerto a la jugada
-            document.querySelector(".jugador img").src ="imagenes/"+ value + ".png";
-            escogeUsuario=value;
-        })   
+            //modifica la imagen del dom de acuerto a la jugada
+            document.querySelector(".jugador img").src = "imagenes/" + value + ".png";
+            escogeUsuario = value;
+        })
     }
 }
 
@@ -52,19 +51,30 @@ jugador()
 
 
 function verSiGano(opJug, opPC) {
-    switch (opPC) {
-        case opJug:
-            console.log ("empate");
-            break;
-        case "piedra":
-            
-        break;
-        case "tijera":
-            
-        break;
-    
-        default:
-            break;
+
+    if ((opJug == "tijera" && opPC == "papel") || (opJug == "papel" && opPC == "piedra") || (opJug == "piedra" && opPC == "tijera")) {
+        console.log("gano")
+        resultado("gano")
+    } else if (opJug == opPC) {
+        console.log("Empate")
+    } else {
+        console.log("pierde")
+        resultado("pierde")
     }
 }
 
+
+function resultado(event) {
+    if (event == "gano") {
+        document.querySelector("body").classList.add("gano");
+        setTimeout(function () {
+            document.querySelector("body").classList.toggle("gano")
+        }, 200)
+    } else if (event == "pierde") {
+        document.querySelector("body").classList.add("game-over");
+        setTimeout(function () {
+            document.querySelector("body").classList.toggle("game-over")
+        }, 200)
+    }
+
+}
